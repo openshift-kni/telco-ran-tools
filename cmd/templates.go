065587aa810cf981be952d4b960d443c550f279a
Ricardo Carrillo Cruz <ricarril@redhat.com>
2022-07-28 12:16:50 +0200

    Install AI images via ImageSet
    This will probably need to be pinned per version, but leaving them as is
    for now.

--- cmd/templates.go
+++ cmd/templates.go
@@ -11,5 +11,6 @@ mirror:
       minVersion: {{ .Version }}
       maxVersion: {{ .Version }}
   additionalImages:
-    - name: quay.io/edge-infrastructure/assisted-installer-agent:latest
+    - name: registry.redhat.io/multicluster-engine/assisted-installer-agent-rhel8
+    - name: registry.redhat.io/multicluster-engine/assisted-installer-rhel8
 `
