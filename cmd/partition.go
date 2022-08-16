0663d8a5075505a8407aed95af0c4a777745c8bb
Ricardo Carrillo Cruz <ricarril@redhat.com>
2022-08-16 10:58:54 +0200

    Fix the commands help (#26)
    They were still showing the boilerplate default help messages

--- cmd/partition.go
+++ cmd/partition.go
@@ -17,13 +17,7 @@ import (
 // partitionCmd represents the partition command
 var partitionCmd = &cobra.Command{
 	Use:   "partition",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Partitions and formats a disk",
 	Run: func(cmd *cobra.Command, args []string) {
 		device, _ := cmd.Flags().GetString("device")
 		size, _ := cmd.Flags().GetInt("size")
