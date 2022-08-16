cfc1c579747f0566cadb4de00d13990c5448aac4
Ricardo Carrillo Cruz <ricarril@redhat.com>
2022-08-16 13:46:44 +0200

    Add DU operators to template (#27)
    Derive version from channel for the most part, some
    are just in 'stable'.

--- cmd/templates.go
+++ cmd/templates.go
@@ -13,4 +13,35 @@ mirror:
   additionalImages:
     - name: registry.redhat.io/multicluster-engine/assisted-installer-agent-rhel8:v2.0.1-5
     - name: registry.redhat.io/multicluster-engine/assisted-installer-rhel8:v2.0.1-5
+  operators:
+    - catalog: registry.redhat.io/redhat/redhat-operator-index:v{{ .Channel }}
+      full: true
+      packages:
+        - name: odf-lvm-operator
+          channels:
+            - name: 'stable-{{ .Channel }}'
+        - name: performance-addon-operator
+          channels:
+            - name: '{{ .Channel }}'
+        - name: ptp-operator
+          channels:
+            - name: 'stable'
+        - name: sriov-network-operator
+          channels:
+            - name: 'stable'
+        - name: cluster-logging
+          channels:
+            - name: 'stable'
+        - name: ocs-operator
+          channels:
+            - name: 'stable-{{ .Channel }}'
+        - name: local-storage-operator
+          channels:
+            - name: 'stable'
+    - catalog: registry.redhat.io/redhat/certified-operator-index:v{{ .Channel }}
+      full: true
+      packages:
+        - name: sriov-fec
+          channels:
+            - name: 'stable'
 `
