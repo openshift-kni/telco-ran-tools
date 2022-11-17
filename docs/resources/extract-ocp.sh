31713060c10eb531a2f1b47d86cdb93f2ce1bee7
Alberto Losada <alosadag@redhat.com>
2022-11-17 08:26:24 +0100

    Improves extracting time from compressed file to container local storage
    Signed-off-by: Alberto Losada <alosadag@redhat.com>

--- docs/resources/extract-ocp.sh
+++ docs/resources/extract-ocp.sh
@@ -6,26 +6,94 @@ BINARY_FOLDER=/var/mnt
 
 pushd $FOLDER
 
-total_copies=$(sort -u $BINARY_FOLDER/$OCP_RELEASE_LIST | wc -l)  # Required to keep track of the pull task vs total
-current_copy=1
+load_images() {
 
-while read -r line;
-do
-  uri=$(echo "$line" | awk '{print$1}')
-  #tar=$(echo "$line" | awk '{print$2}')
-  podman image exists $uri
-  if [[ $? -eq 0 ]]; then
-      echo "Skipping existing image $tar"
-      echo "Copying ${uri} [${current_copy}/${total_copies}]"
+  declare -A pids # Hash that include the images pulled along with their pids to be monitored by wait command
+  local max_bg=10 # Max number of simultaneous skopeo copies to container storage
+  local total_copies=$(sort -u $BINARY_FOLDER/$OCP_RELEASE_LIST | wc -l)  # Required to keep track of the pull task vs total
+  local current_copy=1
+
+  #remove duplicates
+  sort -u -o $OCP_RELEASE_LIST $OCP_RELEASE_LIST
+
+  while read -r line;
+  do
+    uri=$(echo "$line" | awk '{print$1}')
+    podman image exists $uri
+    if [[ $? -eq 0 ]]; then
+      echo "[INFO] Skipping existing image $tar"
+      echo "[INFO] Copying ${uri} [${current_copy}/${total_copies}]"
       current_copy=$((current_copy + 1))
       continue
-  fi
-  tar=$(echo "$uri" |  rev | cut -d "/" -f1 | rev | tr ":" "_")
-  tar zxvf ${tar}.tgz
-  if [ $? -eq 0 ]; then rm -f ${tar}.gz; fi
-  echo "Copying ${uri} [${current_copy}/${total_copies}]"
-  skopeo copy dir://$(pwd)/${tar} containers-storage:${uri}
-  if [ $? -eq 0 ]; then rm -rf ${tar}; current_copy=$((current_copy + 1)); fi
-done < ${BINARY_FOLDER}/${OCP_RELEASE_LIST}
+    fi
+    tar=$(basename ${uri/:/_})
+    tar --use-compress-program=pigz -xf ${tar}.tgz
+    if [ $? -ne 0 ]; then 
+      echo "[ERROR] Could not extract the image ${tar}.gz. Moving to next image" 
+      failed_copies+=(${tar}) # Failed, then add the image to be retrieved later
+      current_copy=$((current_copy + 1))
+      continue 
+    fi
+    echo "[INFO] Copying ${uri} [${current_copy}/${total_copies}]"
+    skopeo copy dir://$(pwd)/${tar} containers-storage:${uri} -q &
 
-exit 0
+    pids[${uri}]=$! # Keeping track of the PID and container image in case the pull fails
+    max_bg=$((max_bg - 1)) # Batch size adapted 
+    current_copy=$((current_copy + 1)) 
+    if [[ $max_bg -eq 0 ]] || [[ $current_copy -gt $total_copies ]] # If the batch is done, then monitor the status of all pulls before moving to the next batch. If the last images are being pulled wait too.
+    then
+      for img in ${!pids[@]}; do
+        wait ${pids[$img]} # The way wait monitor for each background task (PID). If any error then copy the image in the failed array so it can be retried later
+        if [[ $? != 0 ]]; then
+          echo "[ERROR] Pull failed for container image: ${img} . Retrying later... "
+          failed_copies+=(${img}) # Failed, then add the image to be retrieved later
+        else
+          echo "[INFO] Removing folder for ${img}"
+          img_folder=$(basename ${img/:/_})
+          rm -rf ${img_folder}
+        fi
+      done
+      # Once the batch is processed, reset the new batch size and clear the processes hash for the next one
+      max_bg=10
+      pids=()
+    fi
+  done < ${BINARY_FOLDER}/${OCP_RELEASE_LIST}
+}
+
+retry_images() {
+  echo "[RETRYING]"
+  echo ""
+  local rv=0
+  for failed_copy in ${failed_copies[@]}; do
+    echo "[RETRY] Retrying failed image pull: ${failed_copy}"
+    tar=$(basename ${failed_copy/:/_})
+    tar --use-compress-program=pigz -xf ${tar}.tgz
+    if [ $? -ne 0 ]; then
+      echo "[RETRY ERROR] Could not extract the image ${tar}.gz. Moving to next image"
+      rv=1
+      continue
+    fi
+    skopeo copy --retry-times 10 dir://$(pwd)/${tar} containers-storage:${failed_copy} -q
+    if [[ $? -eq 0 ]]; then  
+      rm -rf ${tar}
+    else
+     echo "[ERROR] Limit number of retries reached. The image could not be pulled: ${failed_copy}"
+     rv=1
+    fi
+  done
+  echo "[INFO] Image load done"
+  return $rv
+}
+
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
+  failed_copies=() # Array that will include all the images that failed to be pulled
+  load_images
+  retry_images # Return 1 if max.retries reached
+  if [[ $? -ne 0 ]]; then
+    echo "[FAIL] ${#failed_copies[@]} images were not precached successfully"
+    exit 1
+  else
+    echo "[SUCCESS] All images were precached"
+    exit 0
+  fi
+fi
