3f88d6a2400d3bafe487ee92ff1f1f579bf478aa
Don Penney <dpenney@redhat.com>
2023-02-23 15:45:48 -0500

    Update update-docs.sh to work with older awk
    The CI job is using an image with an older awk that doesn't support the
    inline replace module. Updated the hack/update-docs.sh to use awk to
    update a temporary file instead, copying it back to the original as
    needed.
    
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- hack/update-docs.sh
+++ hack/update-docs.sh
@@ -2,12 +2,24 @@
 
 set -e
 
+WORKDIR=
+function cleanup {
+    if [ -f "${WORKDIR}" ]; then
+        rm -rf "${WORKDIR}"
+    fi
+}
+
+trap cleanup EXIT
+
+WORKDIR=$(mktemp --tmpdir -d doc-update.XXXX)
+
 REPODIR=$(readlink -f "$(dirname $0)/..")
 
 BOOT_IGN="${REPODIR}/docs/resources/boot-beauty.ign"
 DISCOVERY_IGN="${REPODIR}/docs/resources/discovery-beauty.ign"
 
 ZTP_PRECACHING="${REPODIR}/docs/ztp-precaching.md"
+TMP_ZTP_PRECACHING="${WORKDIR}/ztp-precaching.md"
 
 for f in "${BOOT_IGN}" "${DISCOVERY_IGN}" "${ZTP_PRECACHING}"; do
     if [ ! -f "${f}" ]; then
@@ -18,23 +30,26 @@ done
 
 # Update ztp-precaching.md with extraction script changes, if needed
 echo "Updating ${ZTP_PRECACHING}"
+
 # shellcheck disable=SC2016
-sed -i \
-    -e '/``` { .json title=discovery-beauty.ign }/,/```/c``` { .json title=discovery-beauty.ign }\ninsert-discovery-beauty.ign-here\n```' \
+sed -e '/``` { .json title=discovery-beauty.ign }/,/```/c``` { .json title=discovery-beauty.ign }\ninsert-discovery-beauty.ign-here\n```' \
     -e '/``` { .json title=boot-beauty.ign }/,/```/c``` { .json title=boot-beauty.ign }\ninsert-boot-beauty.ign-here\n```' \
-    "${ZTP_PRECACHING}" && \
-awk -i inplace '
-    {
+    "${ZTP_PRECACHING}" > "${TMP_ZTP_PRECACHING}.sed"
+
+awk \
+    -v ai_override="ignitionConfigOverride: '$(jq -c <"${DISCOVERY_IGN}" | sed 's/\\/\\\\/g')'" \
+    -v ocp_override="ignitionConfigOverride: '$(jq -c <"${BOOT_IGN}" | sed 's/\\/\\\\/g')'" \
+    -v discovery_override="$(sed 's/\\/\\\\/g' ${DISCOVERY_IGN})" \
+    -v boot_override="$(sed 's/\\/\\\\/g' ${BOOT_IGN})" \
+    '{
         sub(/ignitionConfigOverride.*extract-ai.*/,ai_override)
         sub(/ignitionConfigOverride.*extract-ocp.*/,ocp_override)
         sub(/insert-discovery-beauty.ign-here/,discovery_override)
         sub(/insert-boot-beauty.ign-here/,boot_override)
         print
     }' \
-    ai_override="ignitionConfigOverride: '$(jq -c <"${DISCOVERY_IGN}" | sed 's/\\/\\\\/g')'" \
-    ocp_override="ignitionConfigOverride: '$(jq -c <"${BOOT_IGN}" | sed 's/\\/\\\\/g')'" \
-    discovery_override="$(sed 's/\\/\\\\/g' ${DISCOVERY_IGN})" \
-    boot_override="$(sed 's/\\/\\\\/g' ${BOOT_IGN})" \
-    "${ZTP_PRECACHING}"
+    "${TMP_ZTP_PRECACHING}.sed" > "${TMP_ZTP_PRECACHING}.awk"
 
-
+if ! cmp -s "${TMP_ZTP_PRECACHING}.awk" "${ZTP_PRECACHING}"; then
+    mv "${TMP_ZTP_PRECACHING}.awk" "${ZTP_PRECACHING}"
+fi
