0b067d149afb6a809ad22d36e4fdf305d6e6f4e5
Don Penney <dpenney@redhat.com>
2023-05-24 14:58:07 -0400

    Add regression test suite
    This update adds a regression test suite to the telco-ran-tools image,
    which will run a series of download tests to verify existing
    functionality, such as invalid parameter handling and operator version
    checking.
    
    Signed-off-by: Don Penney <dpenney@redhat.com>

---
+++ regression/regression-test-invalid-version-format.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Runs invalid parameter handling tests
+#
+
+# Run the command, capturing the output and RC
+factory-precaching-cli download \
+    --testmode \
+    -f "${TESTFOLDER}" \
+    --mce-version 2.2.0 \
+    -r 4.12.notaversion \
+    >& command-output.txt
+rc=$?
+
+# We expect this command to return a non-zero status
+if [ "${rc}" -eq 0 ]; then
+    echo "Expected a non-zero value, but got rc=${rc}"
+    exit 1
+fi
+
+# Check for expected error message
+if ! grep -q 'Error: Invalid release specified. X.Y.Z format expected: 4.12.notaversion' command-output.txt ; then
+    echo "Expected error message not found in command output."
+    exit 1
+fi
+
+exit 0
---
+++ regression/regression-test-stale-cleanup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Runs invalid parameter handling tests
+#
+
+source /usr/local/bin/regression-suite-functions.sh
+
+# Create a fake image file for stale cleanup test
+echo "Test image" > "${TESTFOLDER}"/not-a-real-image@sha256_1234567890123456789012345678901234567890123456789012345678901234.tgz
+
+# Run the command, capturing the output and RC
+factory-precaching-cli download \
+    --testmode \
+    -f "${TESTFOLDER}" \
+    --mce-version 2.2.0 \
+    -r 4.12.15 \
+    -s \
+    >& command-output.txt
+rc=$?
+
+# We expect this command to return a zero status
+if [ "${rc}" -ne 0 ]; then
+    cat command-output.txt
+    echo "Command returned rc=${rc}"
+    exit 1
+fi
+
+# Verify all expected image files exist
+if ! verify_downloaded_files ; then
+    exit 1
+fi
+
+if ! check_for_stale_image_files ; then
+    echo "Stale files detected"
+    exit 1
+fi
+
+exit 0
